312817ba2eb10850529ff41232ac4a5938531f40
Nisse <119867411+NisseMM@users.noreply.github.com>
2026-07-10 00:26:44 +0200

    feat: added Viewport and Color for clearing to render object

--- engine/include/rendering/renderer.h
+++ engine/include/rendering/renderer.h
@@ -1,16 +1,47 @@
 #ifndef RENDERER_H
 #define RENDERER_H
 
+#include <cstddef>
+
+// Window forward declare
 class Window;
+
+struct Viewport
+{
+    int x;
+    int y;
+    int width;
+    int height;
+};
+
+struct Color
+{
+    float r;
+    float g;
+    float b;
+    float a;
+};
+
+// Default values for Viewport and Color
+static constexpr Viewport DEFAULT_VIEWPORT{0, 0, 1280, 720};
+static constexpr Color DEFAULT_COLOR{0.f, 0.f, 0.f, 1.f};
 
 class Renderer
 {
 public:
     Renderer();
+    Renderer(Color const& color);
+    Renderer(Viewport const& viewport);
+    Renderer(Color const& color, Viewport const& viewport);
+
+    Renderer& setViewport(Viewport const& viewport);
+    Renderer& setClearColor(Color const& color);
 
     void render(Window const &window) const;
 
 private:
+    Color clear_color_;
+    Viewport viewport_;
 };
 
 #endif // RENDERER_H
--- engine/src/rendering/renderer.cc
+++ engine/src/rendering/renderer.cc
@@ -4,10 +4,37 @@
 #include <glad/gl.h>
 
 Renderer::Renderer()
+    : Renderer{DEFAULT_COLOR, DEFAULT_VIEWPORT}
 {}
+
+Renderer::Renderer(Color const& color)
+    : Renderer{color, DEFAULT_VIEWPORT}
+{}
+
+Renderer::Renderer(Viewport const& viewport)
+    : Renderer{DEFAULT_COLOR, viewport}
+{}
+
+Renderer::Renderer(Color const& color, Viewport const& viewport)
+    : clear_color_{color}, viewport_{viewport}
+{}
+
+Renderer& Renderer::setViewport(Viewport const& viewport)
+{
+    viewport_ = viewport;
+    return *this;
+}
+
+Renderer& Renderer::setClearColor(Color const& color)
+{
+    clear_color_ = color;
+    return *this;
+}
 
 void Renderer::render(Window const& window) const
 {
     window.makeContextCurrent();
+    glViewport(viewport_.x, viewport_.y, viewport_.width, viewport_.height);
+    glClearColor(clear_color_.r, clear_color_.g, clear_color_.b, clear_color_.a);
     glClear(GL_COLOR_BUFFER_BIT);
 }
