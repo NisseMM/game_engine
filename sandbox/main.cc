42dc49c8a1048df05d780afe136d067bc517a326
Nisse <119867411+NisseMM@users.noreply.github.com>
2026-07-12 02:09:52 +0200

    test: changed test code to draw triangles using mesh object and index buffers

--- sandbox/main.cc
+++ sandbox/main.cc
@@ -1,8 +1,15 @@
 #include <core/window.h>
 #include <rendering/renderer.h>
 #include <rendering/shader.h>
+#include <rendering/vertex_array.h>
+#include <rendering/vertex_buffer.h>
+#include <rendering/vertex_layout.h>
+#include <rendering/index_buffer.h>
+#include <rendering/mesh.h>
 
 #include <iostream>
+
+#include <math/math.h>
 
 int main()
 {
@@ -16,11 +23,30 @@ int main()
 
     ShaderProgram shader{ShaderStage{"assets/shaders/basic.frag", ShaderType::FRAG}, ShaderStage{"assets/shaders/basic.vert", ShaderType::VERT}};
 
+    VertexBuffer vbo{
+        0, 1, 0,
+        -1, 0, 0,
+        1, 0, 0,
+        0, -1, 0};
+    
+    VertexLayout layout{};
+    layout.addAttrib<glm::vec3>();
+    
+    VertexArray vao{};
+    vao.addBuffer(vbo, layout);
+
+    IndexBuffer ebo{
+        0, 1, 2,
+        1, 3, 2};
+
+    Mesh mesh{std::move(vbo), layout, std::move(ebo)};
+
     while (not window.shouldClose())
     {
         renderer.render(window);
         shader.bind();
-        
+        mesh.draw();
+
         window.update();
     }
 }
